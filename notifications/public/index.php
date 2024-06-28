1abdf3139114df448cd546b664e96842c5695aaf
loice <nyandiakamau24@gmail.com>
2024-06-28 12:49:17 +0300

    vercel

--- notifications/public/index.php
+++ notifications/public/index.php
@@ -9,9 +9,17 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+// // Register the Composer autoloader...
+// require __DIR__.'/../vendor/autoload.php';
+
+// // Bootstrap Laravel and handle the request...
+// (require_once __DIR__.'/../bootstrap/app.php')
+//     ->handleRequest(Request::capture());
+
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../../owlcrest/vendor/autoload.php';
+
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once __DIR__.'/../../owlcrest/bootstrap/app.php')
     ->handleRequest(Request::capture());
