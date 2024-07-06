c834aa6ac920198adc41158a86fbcbc120878671
unknown <nyandiakamau24@gmail.com>
2024-07-06 20:48:59 +0300

    vercel

--- notifications/app/Providers/RouteServiceProvider.php
+++ notifications/app/Providers/RouteServiceProvider.php
@@ -81,19 +81,10 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    // protected function mapApiRoutes()
-    // {
-    //     Route::prefix('api')
-    //          ->middleware('api')
-    //          ->namespace($this->namespace)
-    //          ->group(base_path('routes/api.php'));
-    // }
     protected function mapApiRoutes()
-{
-    // Route::middleware('api')
-    //      ->namespace($this->namespace)
-    //      ->group(base_path('routes/api.php'));
-    Route::namespace($this->namespace)
+    {
+        Route::middleware('api')
+             ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
-}
+    }
 }
--- notifications/routes/api.php
+++ notifications/routes/api.php
@@ -16,16 +16,18 @@ Route::middleware(['api'])->group(function () {
 });
 // Route::get('/notification', [NotificationController::class, 'index']);
 // routes/api.php
-Route::post('/notifications/reward', [RewardNotificationController::class, 'send']);
+// Route::post('/notifications/reward', [RewardNotificationController::class, 'send']);
 Route::get('/notification', [NotificationController::class, 'index']);
-Route::post('/notifications/reward', [RewardNotificationController::class, 'send']);
 
-Route::group(['middleware' => ['jwt.verify']], function() {
+
+Route::group(['prefix' => 'v1'], function(){
+Route::group(['middleware' => ['jwt.verify', 'api']], function() {
 // Route::get('/notification', [NotificationController::class, 'index']);
 
+Route::post('/notifications/reward', [RewardNotificationController::class, 'send']);
 Route::post('/notifications/disbursement', [DisbursementNotificationController::class, 'send']);
 Route::post('/notifications/advance-request', [AdvanceRequestNotificationController::class, 'send']);
 Route::post('/notifications/payment', [PaymentNotificationController::class, 'send']);
 });
 
-
+});
