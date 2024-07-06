de6e655b54fd5cbad8568792534300e048e5ad7e
unknown <nyandiakamau24@gmail.com>
2024-07-06 23:26:06 +0300

    vercel

--- notifications/app/Providers/RouteServiceProvider.php
+++ notifications/app/Providers/RouteServiceProvider.php
@@ -84,6 +84,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('v1') 
+        ->middleware('api')
         ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
