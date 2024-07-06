bb2347dd84305da27ca20b47929d8254bfb368b4
unknown <nyandiakamau24@gmail.com>
2024-07-06 18:34:19 +0300

    vercel

--- notifications/app/Providers/RouteServiceProvider.php
+++ notifications/app/Providers/RouteServiceProvider.php
@@ -90,8 +90,10 @@ class RouteServiceProvider extends ServiceProvider
     // }
     protected function mapApiRoutes()
 {
-    Route::middleware('api')
-         ->namespace($this->namespace)
-         ->group(base_path('routes/api.php'));
+    // Route::middleware('api')
+    //      ->namespace($this->namespace)
+    //      ->group(base_path('routes/api.php'));
+    Route::namespace($this->namespace)
+             ->group(base_path('routes/api.php'));
 }
 }
