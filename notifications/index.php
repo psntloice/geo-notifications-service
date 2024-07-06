7ca2a0635c268fd8188f7be1062fe86176f4443f
unknown <nyandiakamau24@gmail.com>
2024-07-06 12:16:11 +0300

    vercel

--- notifications/index.php
+++ notifications/index.php
@@ -16,4 +16,4 @@
 // putenv("APP_URL=https://$vercelURL");
 
 // Forward Vercel requests to public index.
-require __DIR__ . "/" . "../public/index.php";
+require __DIR__ . "/" . "public/index.php";
