a8c05dc93aae02fb463d985b4ecc6eeb4894309a
unknown <nyandiakamau24@gmail.com>
2024-07-07 16:03:06 +0300

    vercel

--- notifications/app/Http/Kernel.php
+++ notifications/app/Http/Kernel.php
@@ -13,6 +13,55 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
+    protected $middleware = [
+        // \App\Http\Middleware\TrustHosts::class,
+        // \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        // \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        // \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array<string, array<int, class-string|string>>
+     */
+    protected $middlewareGroups = [
+        // 'web' => [
+        //     \App\Http\Middleware\EncryptCookies::class,
+        //     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        //     \Illuminate\Session\Middleware\StartSession::class,
+        //     \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        //     \App\Http\Middleware\VerifyCsrfToken::class,
+        //     \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        // ],
+
+        'api' => [
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    /**
+     * The application's middleware aliases.
+     *
+     * Aliases may be used instead of class names to conveniently assign middleware to routes and groups.
+     *
+     * @var array<string, class-string|string>
+     */
+    protected $middlewareAliases = [
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    ];
     // protected $middleware = [
     //     \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
     //     \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -26,22 +75,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $middlewareGroups = [
-        // 'web' => [
-        //     \App\Http\Middleware\EncryptCookies::class,
-        //     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        //     \Illuminate\Session\Middleware\StartSession::class,
-        //     \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        //     \App\Http\Middleware\VerifyCsrfToken::class,
-        //     \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        // ],
-
-        'api' => [
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        
-        ],
-    ];
+    
 
     /**
      * The application's route middleware.
@@ -50,21 +84,18 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    // protected $routeMiddleware = [
-    //     'auth' => \App\Http\Middleware\Authenticate::class,
-    //     'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-    //     'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    //     'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-    //     'can' => \Illuminate\Auth\Middleware\Authorize::class,
-    //     'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-    //     'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-    //     'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-    //     'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-    //     'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    //     'cors' => \App\Http\Middleware\HandleCors::class, // Added in Laravel 10
-    // ];
     protected $routeMiddleware = [
-        // ...
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // 'cors' => \App\Http\Middleware\HandleCors::class, // Added in Laravel 10
+    
         'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
     ];
+    
 }
