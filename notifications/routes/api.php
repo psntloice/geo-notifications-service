5d79b6ee7022619cb39679faff0cf6b608483d0f
unknown <nyandiakamau24@gmail.com>
2024-07-07 16:14:09 +0300

    vercel

--- notifications/routes/api.php
+++ notifications/routes/api.php
@@ -19,14 +19,12 @@ Route::get('/notification', [NotificationController::class, 'index']);
 
 
 Route::group(['prefix' => 'v1'], function(){
-    Route::middleware(['api'])->group(function () {
             Route::get('/notifications', [NotificationController::class, 'index']);
             Route::get('/notification', [NotificationController::class, 'index']);
             Route::post('/notifications/reward', [RewardNotificationController::class, 'send']);
             Route::post('/notifications/disbursement', [DisbursementNotificationController::class, 'send']);
             Route::post('/notifications/advance-request', [AdvanceRequestNotificationController::class, 'send']);
             Route::post('/notifications/payment', [PaymentNotificationController::class, 'send']);
-        });
 Route::group(['middleware' => ['jwt.verify']], function() {
 // Route::get('/notification', [NotificationController::class, 'index']);
 Route::get('/notification', [NotificationController::class, 'index']);
