eaf10f9d40172754cf175a8a93e9b1909cb6f510
unknown <nyandiakamau24@gmail.com>
2024-07-07 13:58:00 +0300

    vercel

--- notifications/app/Http/Kernel.php
+++ notifications/app/Http/Kernel.php
@@ -39,8 +39,7 @@ class Kernel extends HttpKernel
         'api' => [
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            // \App\Http\Middleware\JwtMiddleware::class,
-            'jwt.verify',
+        
         ],
     ];
 
