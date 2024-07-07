23f407c112a552be30e2e489cf0daa810a61dec9
unknown <nyandiakamau24@gmail.com>
2024-07-07 03:13:44 +0300

    vercel

--- notifications/app/Providers/RouteServiceProvider.php
+++ notifications/app/Providers/RouteServiceProvider.php
@@ -81,7 +81,7 @@ $this->mapWebRoutes();
     {
         Log::info('Def API and Web routes');
 
-        Route::middleware('jwt.verify')
+        Route::middleware('api')
         ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
